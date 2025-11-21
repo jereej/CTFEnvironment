a6367f58f7b5e401462a63eeadf2b21ea4ba26a5
Markus Teuhola <markus.teuhola@gmail.com>
2025-11-21 19:37:00 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://deployments-baguette_backend-1:8000',
         changeOrigin: true,
       },
     },
