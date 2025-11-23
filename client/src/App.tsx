1578a5ff435295d9756170f2a5c04f69fb459976
Markus Teuhola <mteuhola19@student.oulu.fi>
2025-11-23 18:02:56 +0200

    #14 Add the Not Found page to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,8 @@ import NavBar from './NavBar';
 import AdminLogin from './AdminLogin';
 import AdminMenu from './AdminMenu';
 import AdminOrders from './AdminOrders';
+// Other components
+import NotFound from './NotFound';
 
 const App: React.FC = () => {
   return (
@@ -22,6 +24,8 @@ const App: React.FC = () => {
       <Route path="/admin" element={<AdminLogin />} />
       <Route path="/admin/menu" element={<AdminMenu />} />
       <Route path="/admin/orders" element={<AdminOrders />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
     </>
   );
---
+++ client/src/NotFound.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Page Not Found</h1>
+      <div className="mb-8">
+        <img 
+          src="/notfound.png" 
+          alt="Restaurant Logo"
+          style={{ width: '600px', height: '400px', objectFit: 'contain', borderRadius: '95%' }}
+        />
+      </div>
+      <p className="text-xl text-gray-600 mb-4">Oh no! This page is not found!</p>
+
+      <Link
+        to="/"
+        className="px-6 py-3 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition"
+      >
+        Return Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
