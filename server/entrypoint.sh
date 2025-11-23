074a83ca5e8c2d05c6b9e2bd4117978cf0436b43
Markus Teuhola <mteuhola19@student.oulu.fi>
2025-11-23 18:36:51 +0200

    Attempt of implementing django migrate

---
+++ server/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Apply database migrations
+python manage.py migrate --noinput
+
+# Collect static files (optional at runtime)
+python manage.py collectstatic --noinput || true
+
+# Start Gunicorn
+exec gunicorn project.wsgi:application \
+    --bind 0.0.0.0:$PORT \
+    --workers 4 \
+    --timeout 120 \
+    --keep-alive 120 \
+    --access-logfile - \
+    --error-logfile -
