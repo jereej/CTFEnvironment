4ef8fd4c0147ea4b9c65fe2638397b962e850890
Markus Teuhola <mteuhola19@student.oulu.fi>
2025-11-23 19:13:42 +0200

    Attempst of adding Postgres
    god have mercy

--- server/project/settings.py
+++ server/project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,10 +83,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="postgres://app:app@postgres:5432/app"
+    )
 }
 
 
