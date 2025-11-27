70946a71b577cf5a32cd80142c4d536b100f59cc
Markus Teuhola <mteuhola19@student.oulu.fi>
2025-11-27 11:26:00 +0200

    Add development parameters

--- server/project/settings.py
+++ server/project/settings.py
@@ -83,9 +83,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="postgres://app:app@postgres:5432/app"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
