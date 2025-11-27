d830c016542e7f08ad34a5d11dcad7acd7d048db
Jere Jacklin <jere.jacklin@gmail.com>
2025-11-27 18:19:38 +0200

    Add OMS template

---
+++ client/src/OMS.tsx
@@ -0,0 +1,16 @@
+//import React, { useState } from "react";
+//import { Link, useNavigate } from 'react-router-dom';
+
+const OMS: React.FC = () => {
+
+return (
+    <div className="absolute inset-0 bg-[#1C1C1DFF]">
+    {/* Overlay */}
+        {/* Content */}
+        <div className="absolute min-h-screen w-full bg-[url('/mail.png')] bg-contain bg-center bg-no-repeat">
+        </div>
+    </div>
+);
+};
+
+export default OMS;
